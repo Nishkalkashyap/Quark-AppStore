770861778a02d74f0432fb3a67d0579ad0465d2f
Nishkal <kashyapnishkal@gmail.com>
2019-07-24 18:18:00 +0530

    minor changes to landing page

--- src/components/dashboard/landing.tsx
+++ src/components/dashboard/landing.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../data/routes';
 
 const Landing = () => (
     <div>
-        <h1>Home Page</h1>
-        <p>The Home Page is accessible by every signed in user.</p>
+        <h1>Landing Page</h1>
+        <p>This Page is accessible by everyone.</p>
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
     </div>
 );
 
