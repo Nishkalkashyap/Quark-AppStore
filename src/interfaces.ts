a140004d8a161dd8095426a09b2b0789d53e4ccb
Nishkal <kashyapnishkal@gmail.com>
2019-07-25 17:14:03 +0530

    added release item interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,1 +1,12 @@
-export interface ProjectData { projectName: string, projectId: string; description: string, createdAt: firebase.firestore.Timestamp }
+export interface ProjectData {
+    projectName: string;
+    description: string;
+    projectId: string;//auto set
+    createdAt: firebase.firestore.Timestamp//auto set
+}
+
+export interface ReleaseItem {
+    createdAt : firebase.firestore.Timestamp;//auto set
+    updatedAt : firebase.firestore.Timestamp;//auto set
+    versionId : string;//auto set
+}
