23b3d7855133621401b14f5edfc633f22adeeb03
Nishkal <kashyapnishkal@gmail.com>
2019-07-25 15:42:56 +0530

    minor changes

--- src/components/account/projects-list.tsx
+++ src/components/account/projects-list.tsx
@@ -84,26 +84,6 @@ TablePaginationActions.propTypes = {
     rowsPerPage: PropTypes.number.isRequired,
 };
 
-// function createData(name: string, calories: number, fat: number) {
-//     return { name, calories, fat };
-// }
-
-// const rows = [
-//     createData('Cupcake', 305, 3.7),
-//     createData('Donut', 452, 25.0),
-//     createData('Eclair', 262, 16.0),
-//     createData('Frozen yoghurt', 159, 6.0),
-//     createData('Gingerbread', 356, 16.0),
-//     createData('Honeycomb', 408, 3.2),
-//     createData('Ice cream sandwich', 237, 9.0),
-//     createData('Jelly Bean', 375, 0.0),
-//     createData('KitKat', 518, 26.0),
-//     createData('Lollipop', 392, 0.2),
-//     createData('Marshmallow', 318, 0),
-//     createData('Nougat', 360, 19.0),
-//     createData('Oreo', 437, 18.0),
-// ].sort((a, b) => (a.calories < b.calories ? -1 : 1));
-
 const useStyles2 = makeStyles(theme => ({
     root: {
         width: '100%',
@@ -129,7 +109,7 @@ const StyledTableCell = withStyles(theme => ({
 
 export default function CustomPaginationActionsTable(props: basePropType) {
 
-    let rowss: { projectName: string, description: string }[] = [];
+    let rowss: { projectName: string, description: string, createdAt : string }[] = [];
     const [rows, setArr] = useState(rowss);
 
     const user = props.firebase.auth.currentUser!;
