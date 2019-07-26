f3c23b5316bb583ab297bfa78b40629352fd5a98
Nishkal <kashyapnishkal@gmail.com>
2019-07-26 22:49:21 +0530

    added font and docs.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './docs.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Firebase, FirebaseContext } from './providers/firebase-provider';
