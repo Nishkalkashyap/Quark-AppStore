0c1eaaad49afb3f0e17545b6a3a303f034a90e35
Nishkal <kashyapnishkal@gmail.com>
2019-08-03 16:44:22 +0530

    added swag-bg svg

--- src/components/swag-background-component.tsx
+++ src/components/swag-background-component.tsx
@@ -9,8 +9,7 @@ export function SwagBackgroundComponent() {
         <div style={{ position: 'absolute', zIndex: -2 }} className="swag-background">
             {
                 svgs.map((_svg, index) => {
-                    return (<svg key={_svg.svg.shape + index} dangerouslySetInnerHTML={{ __html: _svg.svg.shape }} className={`${_svg.svg.className}`} viewBox={_svg.svg.viewBox} style={_svg.style}>
-                    </svg>)
+                    return (<svg key={_svg.svg.shape + index} dangerouslySetInnerHTML={{ __html: _svg.svg.shape }} className={`${_svg.svg.className} random-svg`} viewBox={_svg.svg.viewBox} style={_svg.style} />)
                 })
             }
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './docs.css';
+import './styles/swag-background.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Firebase, FirebaseContext } from './providers/firebase-provider';
