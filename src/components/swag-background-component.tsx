b90711ac075538ac69b8006c8d52056922e98956
Nishkal <kashyapnishkal@gmail.com>
2019-08-04 18:17:22 +0530

    minor changes

--- src/components/swag-background-component.tsx
+++ src/components/swag-background-component.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { getSvgs, darkSvgs } from '../data/svgs';
 /* eslint import/no-webpack-loader-syntax: off */
 import '!!style-loader!css-loader!stylus-loader!./../styles/mixins.styl';
+import { COLORS } from '../util';
 
 export function SwagBackgroundComponent() {
 
     const svgs = getSvgs().concat(darkSvgs);
+    // const dotColor = COLORS.PRIMARY;
+    const dotColor = `#999999`;
 
     return (
         <div style={{ position: 'absolute', width: '100%', height: '100vh', zIndex: -5 }}>
@@ -17,7 +20,7 @@ export function SwagBackgroundComponent() {
                 }
             </div> */}
             <div style={{ position: 'absolute', height: '100%', zIndex: -4, width: '100%', background: `linear-gradient(to bottom, #ffffff00, #ffffff)` }}></div>
-            <div style={{ position: 'absolute', height: '100%', zIndex: -5, width: '100%', backgroundImage: `radial-gradient(#999999 1px, transparent 1px), radial-gradient(#999999 1px, transparent 1px)`, backgroundPosition: '0 0, 25px 25px', backgroundSize: '50px 50px' }}> </div>
+            <div style={{ position: 'absolute', height: '100%', zIndex: -5, width: '100%', backgroundImage: `radial-gradient(${dotColor} 1px, transparent 1px), radial-gradient(${dotColor} 1px, transparent 1px)`, backgroundPosition: '0 0, 25px 25px', backgroundSize: '50px 50px' }}> </div>
             {/* <div style={{ position: 'absolute', height: '100%', zIndex: -5, width: '100%', background: `url("${dots}")`, backgroundSize: '30px' }}> </div> */}
         </div>
     )
