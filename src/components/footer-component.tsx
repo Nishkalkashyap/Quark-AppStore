b2c9f980f4975bf516af4d596dc55d5e2bf22664
Nishkal <kashyapnishkal@gmail.com>
2019-08-09 18:53:25 +0530

    added links in footer

--- src/components/footer-component.tsx
+++ src/components/footer-component.tsx
@@ -1,45 +1,73 @@
 import React from 'react'
-import { Typography, Link, Button } from '@material-ui/core';
+import { Typography, Link } from '@material-ui/core';
 import { COLORS } from '../util';
-import { basePropType } from '../basePropType';
-import { StandardProperties, WritingModeProperty, SvgProperties } from 'csstype';
-import CardBgComponent from './main-background-component';
+import { StandardProperties } from 'csstype';
 
 export function FooterComponent() {
     return (
-        <footer style={{ textAlign: 'center', marginTop: '100px', position: 'relative', padding: '50px 0px' }}>
-            <Typography variant="body2">
-                You've hit rock bottom. Find an issue?
-                <Link target="_blank" href="https://github.com/Nishkalkashyap/Quark-appstore" style={{ cursor: 'pointer', marginLeft: '5px' }}>Submit a fix on Github</Link>
-            </Typography>
-            <div>
-                <Typography variant="body2">
-                    Copyright © 2019 Nishkal Kashyap. All rights reserved.
+        <footer style={footerStyle}>
+            <div style={linksDivStyle}>
+                <Typography style={{ marginBottom: '15px' }}>
+                    GitHub
                 </Typography>
+                <Link target="_blank" href="https://github.com/Nishkalkashyap/Quark-electron" style={linkStyle}>Quark: Build</Link>
+                <Link target="_blank" href="https://github.com/Nishkalkashyap/Quark-docs" style={linkStyle}>Quark: Docs</Link>
+                <Link target="_blank" href="https://github.com/Nishkalkashyap/Quark-appstore" style={linkStyle}>Quark: Appstore</Link>
+                <Link target="_blank" href="https://github.com/Nishkalkashyap/Quark-samples" style={linkStyle}>Quark: Examples</Link>
             </div>
-            <div onClick={() => window.open('https://quarkjs.io/download/')} style={{ display: 'inline-block', color: COLORS.ON_PRIMARY, marginTop: '50px', padding: '20px 40px', backgroundColor: COLORS.PRIMARY, fontSize: '2em', borderRadius: '2px', fontFamily: 'var(--heading-font-family)', cursor: 'pointer' }}>
-                Download Quark
+            <div style={centerDivStyle}>
+                <Typography variant="body2">
+                    You've hit rock bottom. Find an issue?
+                <Link target="_blank" href="https://github.com/Nishkalkashyap/Quark-appstore" style={linkStyle}>Submit a fix on Github</Link>
+                </Typography>
+                <div>
+                    <Typography variant="body2">
+                        Copyright © 2019 Nishkal Kashyap. All rights reserved.
+                </Typography>
+                </div>
+                <div onClick={() => window.open('https://quarkjs.io/download/')} style={{ display: 'inline-block', color: COLORS.ON_PRIMARY, marginTop: '50px', padding: '20px 40px', backgroundColor: COLORS.PRIMARY, fontSize: '1.6em', borderRadius: '2px', fontFamily: 'var(--heading-font-family)', cursor: 'pointer' }}>
+                    Download Quark
+                </div>
             </div>
-            {/* <CardBgComponent /> */}
-            {/* <svg style={svgStyles} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 338.05" preserveAspectRatio="none">
-                <path>
-                    <animate
-                        attributeName="d"
-                        values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z"
-                        repeatCount="indefinite"
-                        dur="30s"
-                    />
-                </path>
-            </svg> */}
+            <div style={linksDivStyle}>
+                <Typography style={{ marginBottom: '15px' }}>
+                    Links
+                </Typography>
+                <Link target="_blank" href="https://medium.com/hackernoon/announcing-quark-a-software-sketchbook-for-your-projects-2f53553415b" style={linkStyle}>Medium</Link>
+                <Link target="_blank" href="https://quarkjs.io" style={linkStyle}>Documentation</Link>
+                <Link target="_blank" href="https://quarkjs.io/FAQ/terms-of-service.html" style={linkStyle}>Terms of service</Link>
+                <Link target="_blank" href="https://quarkjs.io/FAQ/about.html" style={linkStyle}>About</Link>
+                <Link target="_blank" href="mailto:hello@nishkal.in?subject=Hello%20Nishkal" style={linkStyle}>Contact</Link>
+            </div>
         </footer>
     )
 }
 
-const svgStyles: StandardProperties & SvgProperties = {
-    transform: 'scale(1, -0.5)',
-    bottom: '0px',
-    left: '0px',
-    zIndex: -2,
-    position: 'absolute',
-    fill: '#6574cd99'
-};
+const footerStyle: StandardProperties = {
+    padding: '50px 0px',
+    backgroundColor: COLORS.BACKGROUND,
+    color: COLORS.ON_BACKGROUND,
+    borderLeft: `solid 1px ${COLORS.ON_BACKGROUND}33`,
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexWrap : 'wrap',
+    textAlign: 'center',
+    marginTop : '100px'
+}
+
+const centerDivStyle: StandardProperties = {
+    textAlign: 'center',
+    margin: '20px 20px',
+    flexGrow: 2
+}
+
+const linksDivStyle: StandardProperties = {
+    margin: '20px 20px',
+    flexGrow: 1
+}
+
+const linkStyle: StandardProperties = {
+    display: 'block',
+    cursor: 'pointer',
+    margin: '5px 0px'
+}
